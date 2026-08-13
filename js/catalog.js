503296a3df73e7da9bd8a92e4f54ae13201fbfac
ianfangch <ianfangch@users.noreply.github.com>
2026-08-13 11:54:34 +0800

    Restore complete sink specification images

--- js/catalog.js
+++ js/catalog.js
@@ -313,7 +313,9 @@
             '<h2 class="mb-0">Available Models &amp; Specifications</h2></div>' +
             product.catalogueRows.map(function (row) {
                 return '<div class="row g-4 ' + (row.models.length === 1 ? 'align-items-center' : 'align-items-start') + ' py-5 border-bottom">' +
-                    '<div class="col-12">' + row.models.map(function (model, modelIndex) {
+                    '<div class="col-lg-6"><img class="img-fluid d-block" style="width:100%;height:340px;object-fit:contain;object-position:center" src="' +
+                    escapeHtml(product.image) + '" alt="' + escapeHtml(product.imageAlt) + '"></div>' +
+                    '<div class="col-lg-6">' + row.models.map(function (model, modelIndex) {
                         var modelClass = modelIndex === row.models.length - 1 ? 'pb-3 mb-3' : 'pb-3 mb-3 border-bottom';
                         return '<div class="' + modelClass + '"><h2 class="mb-2">' + escapeHtml(model.code) +
                             (model.note ? ' <small class="fs-6 fw-normal">' + escapeHtml(model.note) + '</small>' : '') +
